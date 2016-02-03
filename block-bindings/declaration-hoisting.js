734be3bfd24c904221e35193deb1bbf28e43b626
Lazhari <lazhari.mohammed@outlook.com>
2016-02-03 18:08:50 +0000

    Var declarations and hoisting

---
+++ block-bindings/declaration-hoisting.js
@@ -0,0 +1,15 @@
+function getValue(cond) {
+    if (cond) {
+        var value = "blue";
+
+        // Other code
+
+        return value
+    } else {
+        // value exists here with a value of undefined
+
+        return null;
+    }
+
+    // Value exits here with a value of undefined
+}
