b0a05073232a0785412e984a4b58783bfef2bba5
Lazhari <lazhari.mohammed@outlook.com>
2016-02-03 18:15:40 +0000

    Let declarations

---
+++ block-bindings/let-declarations.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function getValue(condition) {
+    
+    if(condition) {
+        let value = "blue";
+
+        // other code
+
+        return value;
+    } else {
+        //Value doesn't exist here
+        
+        return null
+    }
+
+    // Value does't exist here 
+
+}
